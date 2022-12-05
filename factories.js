c29ff845f32ce79e8b35c6c859282a0e3eb6e129
Jack Graymer <jackgraymer@gmail.com>
2022-12-05 23:13:45 +0100

    add function position and test
    adds the ship object a location based on the start coordinate, the lenght and vertical or horizontal direction

--- factories.js
+++ factories.js
@@ -1,6 +1,7 @@
-const ship = (n) => {
-    let length = n
-    let position = []
+const ship = (length, start = 'A1') => {
+    length = length
+    start = start
+    let location = position(start, length, /* placeholder for direction */ )
     let hits = 0
     function hit () {
         hits ++
@@ -12,11 +13,36 @@ const ship = (n) => {
         }
         return false
     }
-    return {length, position, hits, hit, isSunk}
+    return {length, location, hits, start, hit, isSunk}
+}
+
+function position(start, length = 3, dir = 'horizontal' ) {
+    let coordinates = []
+    dir = dir[0].toLowerCase()
+
+    // Vertical position
+
+    if(dir == 'v'){
+        while (coordinates.length < length){
+            coordinates.push(start)
+            let number = Number(start[1])+1
+            start = start[0] + number 
+        }
+    }
+    if (dir == 'h'){
+        while (coordinates.length < length){
+            coordinates.push(start)
+            let letter = start.charCodeAt(0)+1
+            start = String.fromCharCode(letter) + start[1]
+        }
+    }
+    return coordinates
 }
 
 function board (){
+    // Horizontal Axis
     let x = ['a','b','c','d','e','f','g','h','j','i']
+    // Vertical Axis
     let y = [1,2,3,4,5,6,7,8,9,10]
     let total = []
     for( let coor of x){
@@ -30,7 +56,9 @@ function board (){
 }
 
 const Gameboard = () => {
-    
+    function placeShip(start, direction){
+
+    }
 }
 
-export {ship, board}
+export {ship, position, board}
--- factories.test.js
+++ factories.test.js
@@ -1,10 +1,17 @@
-import {ship, board} from './factories'
+import {ship, position, board} from './factories'
 
 test ('Create boat with factory', () => {
     expect(ship(5).length).toBe(5)
     expect(ship(2).hits).toBe(0)
     expect(ship(4).hit()).toBe(1)
     expect(ship(3).isSunk()).toBe(false)
+    expect(ship(8).start).toBe('A1')
+    expect(ship(4).location).toStrictEqual(['A1', 'B1', 'C1', 'D1'])
+})
+
+test('Ship position vertical and horizontal', () => {
+    expect(position('A1', 4, 'vertical')).toStrictEqual(['A1', 'A2', 'A3', 'A4'])
+    expect(position('B2')).toStrictEqual(['B2', 'C2', 'D2'])
 })
 
 test ('Board cells creation', () => {
