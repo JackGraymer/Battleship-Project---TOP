ea4d5f3ce8b43bcc5fb3f74c789245718b577870
Jack Graymer <jackgraymer@gmail.com>
2022-12-05 20:07:40 +0100

    add board function function and test
    creates 10x10 cells in succesion array: A1,A2...

--- factories.js
+++ factories.js
@@ -1,14 +1,36 @@
 const ship = (n) => {
     let length = n
     let position = []
-    let hit = 0
-    function sunk () {
+    let hits = 0
+    function hit () {
+        hits ++
+        return hits
+    }
+    function isSunk () {
         if(hit == length){
             return true
         }
         return false
     }
-    return {length, position, hit, sunk}
+    return {length, position, hits, hit, isSunk}
 }
 
-export default ship
+function board (){
+    let x = ['a','b','c','d','e','f','g','h','j','i']
+    let y = [1,2,3,4,5,6,7,8,9,10]
+    let total = []
+    for( let coor of x){
+        //makes letters Uppercase (coor X)
+        coor = coor.toUpperCase()
+        for( let num of y){
+            total.push(coor + num)
+        }
+    }
+    return total
+}
+
+const Gameboard = () => {
+    
+}
+
+export {ship, board}
--- factories.test.js
+++ factories.test.js
@@ -1,7 +1,12 @@
-import ship from './factories'
+import {ship, board} from './factories'
 
 test ('Create boat with factory', () => {
     expect(ship(5).length).toBe(5)
-    expect(ship(2).hit).toBe(0)
-    expect(ship(3).sunk()).toBe(false)
+    expect(ship(2).hits).toBe(0)
+    expect(ship(4).hit()).toBe(1)
+    expect(ship(3).isSunk()).toBe(false)
+})
+
+test ('Board cells creation', () => {
+    expect(board().length).toBe(100)
 })
